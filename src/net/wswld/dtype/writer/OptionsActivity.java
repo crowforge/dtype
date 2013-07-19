cf8ca4769baad862b0b6c7da4b49dd2e78908575
Vsevolod Glumov <seva17@gmail.com>
2013-07-19 16:14:45 +0400

    Worked on OptionsActivity javadoc

--- src/net/wswld/dtype/writer/OptionsActivity.java
+++ src/net/wswld/dtype/writer/OptionsActivity.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 
 /**
  * <h1>dType Options Screen</h1>
- * Options screen is represented by the <code>OptionsActivity</code>. It contains eleven buttons:
+ * Options screen is represented by the <code>OptionsActivity</code>. Visually, it contains eleven buttons:
  * <ol>
  *     <li><code>Clear</code> for clearing the text preserved in the <code>doutput_preserved</code>
  *     field of the database. Since the <code>EditText</code> field of the
@@ -20,8 +20,45 @@ import android.widget.Button;
  *     contents of the <code>doutput_preserved</code> in both
  *     {@link DTypeActivity#onCreate(android.os.Bundle)} and {@link DTypeActivity#onResume()} - it
  *     is safe to assume that it is also cleared.</li>
- *     <li><code>BtThemeGrey</code> changes the theme value in the database, therefore changing the
+ *     <li><code>BtThemeGrey</code> calls the {@link #changeTheme(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the color theme of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtThemeBlue</code> calls the {@link #changeTheme(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the theme value in the database, therefore changing the
  *     color theme of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtThemeDark</code> calls the {@link #changeTheme(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the theme value in the database, therefore changing the
+ *     color theme of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtFontSans</code> calls the {@link #changeFont(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the font value in the database, therefore changing the
+ *     typeface of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtFontSerif</code> calls the {@link #changeFont(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the font value in the database, therefore changing the
+ *     typeface of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtFontMono</code> calls the {@link #changeFont(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the font value in the database, therefore changing the
+ *     typeface of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtSizeSmall</code> calls the {@link #changeSize(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the size value in the database, therefore changing the
+ *     text size of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtSizeMid</code> calls the {@link #changeSize(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the size value in the database, therefore changing the
+ *     text size of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>BtSizeBig</code> calls the {@link #changeSize(View v)} method, which changes the theme value in 
+ *     the database, therefore changing the size value in the database, therefore changing the
+ *     text size of {@link DTypeActivity dTypeActivity}.</li>
+ *     <li><code>ReportBug</code> calls {@link #AddBug (View view)} method, which opens URL of a "Report a Bug" page in 
+ *     browser.</li>
+ * </ol>
+ * There are also a couple of methods not really paired with any of the buttons, such as:
+ * <ol>
+ * <li>{@link #OnKeyDown(int keyCode, KeyEvent event)} override, that implements exiting the options screen on 
+ * <code>Menu</code> or <code>Back</code> button pressed. Pretty standard.</li>
+ * <li>{@link #selectedTheme (int Theme, OptionsActivity optionsActivity)} that applies special visual style to the 
+ * active theme button.</li>
+ * <li>{@link #selectedFont (int Theme, OptionsActivity optionsActivity)} that applies special visual style to the 
+ * active font button.</li>
+ * <li>{@link #selectedSize (int Theme, OptionsActivity optionsActivity)} that applies special visual style to the 
+ * active size button.</li>
  * </ol>
  */
 public class OptionsActivity extends DTypeActivity {
@@ -30,13 +67,11 @@ public class OptionsActivity extends DTypeActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.opt);
         
-        /**
-         * Enabling dithering for the whole window to ensure gradients shown right later.
-         */
+        // Enabling dithering for the whole window to ensure gradients shown right later.
 		getWindow().setFormat(PixelFormat.RGBA_8888);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
 		
-		/** Theme selection routine */
+		// Theme selection routine
         SharedPreferences prefs = getSharedPreferences("com.dtype.writer", MODE_PRIVATE);
         int Theme = 1;
 		int Font = 1;
@@ -46,13 +81,13 @@ public class OptionsActivity extends DTypeActivity {
 		Font = prefs.getInt("font", Font);
 		Size = prefs.getInt("size", Size);
 		
-		/** Selecting theme */
+		// Selecting theme
 		selectedTheme(Theme, this);
 		
-		/** Selecting font */
+		// Selecting font
 		selectedFont(Font, this);
 		
-		/** Selecting font size */
+		// Selecting font size
 		selectedSize(Size, this);
 	};
 	
@@ -62,12 +97,12 @@ public class OptionsActivity extends DTypeActivity {
 	@Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 		
-		/** Back to main on 'menu' pressed*/
+		// Back to main on 'menu' pressed
         if (keyCode == KeyEvent.KEYCODE_MENU) {
         	finish();
         };
 		
-        /** Back to main on 'back' pressed*/
+        // Back to main on 'back' pressed
         if (keyCode == KeyEvent.KEYCODE_BACK) {
         	finish();
         };
